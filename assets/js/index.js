1b9076bd962f1eced2b7f328b2c6eec192db3419
Edward Shanahan <edwardshanahan357@gmail.com>
2023-11-30 17:46:53 +0000

    Fetch data to display adivce

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,20 @@
+const getAdvice = async (api) => {
+  const response = await fetch(api);
+  const data = await response.json();
 
+  const { id, advice } = data.slip;
+
+  generateAdvice(id, advice);
+};
+
+const generateAdvice = (id, advice) => {
+  document.getElementById("advice-id").textContent = id;
+  document.getElementById("adive-text").textContent = `"${advice}"`;
+  console.log(id, advice);
+};
+
+document
+  .getElementById("new-advice")
+  .addEventListener("click", () =>
+    getAdvice("https://api.adviceslip.com/advice")
+  );
